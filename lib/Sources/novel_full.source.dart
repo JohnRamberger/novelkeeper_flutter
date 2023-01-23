825c6f59048fed396c9919bcc496eef2282c48c6
JohnRamberger <jramberger3@gatech.edu>
2023-01-22 21:21:16 -0500

    Update novel_full.source.dart

--- lib/Sources/novel_full.source.dart
+++ lib/Sources/novel_full.source.dart
@@ -2,9 +2,10 @@ import 'package:novelkeeper_flutter/Model/source.model.dart';
 import '../Model/scrape_job.model.dart';
 
 class NovelFull extends Source {
-  @override
-  String name = "NovelFull";
-  String baseUrl = "";
+  NovelFull() {
+    name = "NovelFull";
+    baseUrl = "https://novelfull.com";
+  }
 
   @override
   ScrapeJob searchNovelJob({required int page, required String query}) {
