2967056b3bb62eaf143be66c3f8a6a339fae505c
JohnRamberger <jramberger3@gatech.edu>
2023-01-22 22:59:00 -0500

    Update sources.view.dart

--- lib/Views/sources.view.dart
+++ lib/Views/sources.view.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:novelkeeper_flutter/Views/Sources/novel_full.view.dart';
 
 class SourcesView extends StatefulWidget {
   const SourcesView({super.key});
@@ -11,6 +14,16 @@ class SourcesView extends StatefulWidget {
 class _SourcesViewState extends State<SourcesView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      children: [
+        ListTile(
+          title: Text("Novelfull"),
+          onTap: () {
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => NovelfullView()));
+          },
+        )
+      ],
+    );
   }
 }
