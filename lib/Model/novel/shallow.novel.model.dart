9077caf2f38db7b3fe327948342e0e0dafdb4881
JohnRamberger <jramberger3@gatech.edu>
2023-01-22 22:28:08 -0500

    Update shallow.novel.model.dart

--- lib/Model/novel/shallow.novel.model.dart
+++ lib/Model/novel/shallow.novel.model.dart
@@ -2,4 +2,9 @@
 class ShallowNovel {
   String? title, coverUrl, sourceUrl;
   ShallowNovel({this.title, this.coverUrl, this.sourceUrl});
+
+  @override
+  String toString() {
+    return 'ShallowNovel(\n\ttitle: $title, \n\tcoverUrl: $coverUrl, \n\tsourceUrl: $sourceUrl\n)';
+  }
 }
