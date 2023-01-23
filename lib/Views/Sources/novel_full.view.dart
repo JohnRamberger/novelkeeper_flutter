7210f7ad7b8e8d1509a2c10ade8d9b7d34063104
JohnRamberger <jramberger3@gatech.edu>
2023-01-22 22:56:23 -0500

    Create novel_full.view.dart

---
+++ lib/Views/Sources/novel_full.view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NovelfullView extends StatefulWidget {
+  const NovelfullView({super.key});
+
+  @override
+  State<NovelfullView> createState() => _NovelfullViewState();
+}
+
+class _NovelfullViewState extends State<NovelfullView> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("hello");
+  }
+}
