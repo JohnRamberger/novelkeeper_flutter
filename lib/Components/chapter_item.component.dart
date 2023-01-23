4a6aeceb3dea604301c89a1832648c114a666af8
JohnRamberger <jramberger3@gatech.edu>
2023-01-23 14:53:14 -0500

    added Flushbar

--- lib/Components/chapter_item.component.dart
+++ lib/Components/chapter_item.component.dart
@@ -1,3 +1,4 @@
+import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:novelkeeper_flutter/Model/novel/chapter.model.dart';
 
@@ -13,6 +14,19 @@ class ChapterItem extends StatelessWidget {
         chapter.title,
         overflow: TextOverflow.ellipsis,
       ),
+      onLongPress: () {
+        // create popup with full title
+        Flushbar(
+          flushbarPosition: FlushbarPosition.BOTTOM,
+          message: chapter.title,
+          duration: const Duration(seconds: 3),
+          flushbarStyle: FlushbarStyle.FLOATING,
+          backgroundColor: Colors.white,
+          messageColor: Colors.black,
+          margin: const EdgeInsets.all(8),
+          borderRadius: BorderRadius.circular(8),
+        ).show(context);
+      },
     );
   }
 }
