e71fc43d8cede55c03df5b87d2df65fd19c09221
JohnRamberger <jramberger3@gatech.edu>
2023-01-23 01:53:27 -0500

    Create chapter_item.component.dart

---
+++ lib/Components/chapter_item.component.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:novelkeeper_flutter/Model/novel/chapter.model.dart';
+
+class ChapterItem extends StatelessWidget {
+  const ChapterItem({required this.chapter, super.key});
+
+  final Chapter chapter;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(title: Text(chapter.title));
+  }
+}
