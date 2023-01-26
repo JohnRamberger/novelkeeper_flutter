d385c98da78c54eb7b53f79653cf7e1101ba4f50
JohnRamberger <jramberger3@gatech.edu>
2023-01-26 17:38:41 -0500

    update checking

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import "package:flutter/material.dart";
 import 'package:novelkeeper_flutter/app.dart';
 
+import 'package:auto_update/auto_update.dart';
+
 void main() async {
+  // check app version
+  Map<dynamic, dynamic> results =
+      await AutoUpdate.fetchGithub("JohnRamberger", "novelkeeper_flutter");
+  if (results != null) {
+    if (results['assetUrl'] == "up-to-date") {
+      /* aplication is up-to-date */
+      print("up-to-date");
+    } else if (results['assetUrl'].isEmpty()) {
+      /* package or user don't found */
+      print("package or user don't found");
+    } else {
+      /* update url found */
+      print("update url found");
+    }
+  }
+
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <url_launcher_windows/url_launcher_windows.h>
+#include <auto_update/auto_update_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  UrlLauncherWindowsRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
+  AutoUpdatePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AutoUpdatePlugin"));
 }
