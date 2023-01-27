bc89e9db4fc6d58cf07155acaa4bcd23f14f29df
JohnRamberger <jramberger3@gatech.edu>
2023-01-27 15:21:25 -0500

    added path to AppInfoView

--- lib/View/settings.view.dart
+++ lib/View/settings.view.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:novelkeeper_flutter/View/settings/app_info.view.dart';
 
 class SettingsView extends StatefulWidget {
   const SettingsView({super.key});
@@ -11,6 +14,16 @@ class SettingsView extends StatefulWidget {
 class _SettingsViewState extends State<SettingsView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      children: [
+        ListTile(
+          title: const Text('App Info'),
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const AppInfoView()));
+          },
+        )
+      ],
+    );
   }
 }
