89a6101b027f935c3a83b77c696f276d4db19d59
JohnRamberger <jramberger3@gatech.edu>
2023-01-27 15:58:39 -0500

    initialize sqlite

--- lib/Config/config.dart
+++ lib/Config/config.dart
@@ -1,6 +1,26 @@
 import 'package:novelkeeper_flutter/Model/scrape_client.model.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 class NKConfig {
   static const String appName = "Novel Keeper";
   static var scrapeClient = ScrapeClient();
+  static const String dbPath = "novelkeeper.db";
+
+  static Future<Database> database() async {
+    return openDatabase(
+      join(await getDatabasesPath(), dbPath),
+      onCreate: (db, version) {
+        // initialize the db on first creation
+        return db.execute(
+          'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)',
+        );
+      },
+      version: 1,
+    );
+  }
+
+  static Future<void> init() async {
+    // open the db
+  }
 }
