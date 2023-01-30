863c15e293f8fe2b41212220fdb3dfc129b5349a
JohnRamberger <jramberger3@gatech.edu>
2023-01-29 23:33:46 -0500

    Update themes.dart

--- lib/Themes/themes.dart
+++ lib/Themes/themes.dart
@@ -3,8 +3,8 @@ import "package:flutter/material.dart";
 import 'package:novelkeeper_flutter/Themes/theme.model.dart';
 
 NovelKeeperTheme defaultTheme = NovelKeeperTheme(
-  primary: Colors.red,
-  accent: Colors.redAccent,
+  primary: Colors.purple,
+  // accent: Colors.redAccent,
   background: const Color.fromARGB(255, 20, 21, 24),
   backgroundSecondary: const Color.fromARGB(255, 41, 44, 46),
 );
