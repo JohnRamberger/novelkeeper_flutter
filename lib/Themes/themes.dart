e6f7c9776d7ef0f588b6fc49c36a543fd5cf7ddd
JohnRamberger <jramberger3@gatech.edu>
2023-01-29 23:33:16 -0500

    Update themes.dart

--- lib/Themes/themes.dart
+++ lib/Themes/themes.dart
@@ -6,5 +6,5 @@ NovelKeeperTheme defaultTheme = NovelKeeperTheme(
   primary: Colors.red,
   accent: Colors.redAccent,
   background: const Color.fromARGB(255, 20, 21, 24),
-  backgroundSecondary: const Color.fromARGB(255, 70, 77, 87),
+  backgroundSecondary: const Color.fromARGB(255, 41, 44, 46),
 );
